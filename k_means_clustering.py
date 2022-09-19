969320c47eaf9484609d514f8def06c5fa6e92d3
Md. Fazley Rafy <47736100+fazleyrafy@users.noreply.github.com>
2022-09-19 19:03:21 -0400

    Update k_means_clustering.py

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -1,36 +1,35 @@
-import numpy as np
-import sklearn.datasets, sklearn.decomposition
+## importing all the necessary modules
+
 import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.decomposition import PCA
 import tensorflow as tf
-from scipy.linalg import eigh
-from sklearn.preprocessing import StandardScaler
-
-(x_train, y_train), ((x_test, y_test)) = tf.keras.datasets.mnist.load_data()
-x = x_train.reshape(-1,28*28)
-
-print(x_train.shape)
-
-# test_case
-digit = x[32]
-digit.reshape(28,28)
-#plt.title('test_image')
-
 from sklearn.cluster import KMeans
 from sklearn import metrics
 from sklearn.cluster import MiniBatchKMeans
 
-k_means_5cent = KMeans(n_clusters = 5, max_iter = 1000).fit(x)
-k_means_centroid_5 = k_means_5cent.cluster_centers_
-k_means_centroid_5.shape
+## loading the MNIST dataset and reshaping the training dataset for further experiment
+(x_train, y_train), ((x_test, y_test)) = tf.keras.datasets.mnist.load_data()
+x = x_train.reshape(-1,28*28)
 
-axes = []
-fig = plt.figure(1, figsize=(10,10))
+## function to fit the data of x with k_means of # of clusters and setting centroids
+# here n is the number of centroids in n number of clusters
+def apply_kmeans(n):
+    k_means_mnist = KMeans(n_clusters = n, max_iter = 1000).fit(x)
+    k_means_centroid_n = k_means_mnist.cluster_centers_
+    return k_means_centroid_n
 
-for i in range(5):
-    axes.append(fig.add_subplot(1,5, i+1) )
-    axes[i].set_title("Centroid" + str(i+1))
-    plt.imshow(k_means_centroid_5[i].reshape(28,28), cmap='pink')
-fig.tight_layout()
-plt.show()
+## function to display the images of # of centroids
+def display_kmeans(n, arr):
+    axes = []
+    fig = plt.figure(2, figsize=(12,12))
+    for i in range(n):
+        if(n<=2):    
+            axes.append(fig.add_subplot(1,2, i+1))
+        elif(n>2):
+            axes.append(fig.add_subplot(5,6, i+1))
+        axes[i].set_title("Centroid" + str(i+1))
+        plt.imshow(arr[i].reshape(28,28), cmap='pink')
+
+## results for only 30 centroids with images
+
+temp = apply_kmeans(30)
+display_kmeans(30, temp)
